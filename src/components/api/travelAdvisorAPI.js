1c86f74c559b7de718117db84ed1280098a4e76c
Divyanshi Singh Bora <82605925+thesilentline@users.noreply.github.com>
2022-07-01 20:15:00 +0530

    Add files via upload

---
+++ src/components/api/travelAdvisorAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': '4992c192c6mshecf33ba7e792672p14eefcjsn1d4aa7963e41',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
